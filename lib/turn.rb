5455a4f1e33336174a07bd80b18b74fac6cad09b
Emma Freeman <freemoe04@gmail.com>
2017-12-08 20:50:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,7 +5,7 @@ def display_board(board)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
- 
+
 def input_to_index(input)
    input.to_i - 1
 end
@@ -13,4 +13,3 @@ end
 def move(board, index, token = "X")
    board[index] = token
 end
-
