34ccbc20e1fe5756620c6b7c489b826b22cde24c
Emma Freeman <freemoe04@gmail.com>
2017-12-08 20:57:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,6 +14,30 @@ def move(board, index, token = "X")
    board[index] = token
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-end 
+def position_taken?(board, index)
+   if board[index] == " " || board[index] == nil
+     false
+   else
+     true
+   end
+ end
+ 
+ def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+     true
+   else
+    false
+   end
+ end
+ 
+ def turn(board)
+   puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+  if valid_move?(board, index) == true
+     move(board, index, token = "X")
+   else
+     turn(board)
+   end
+   display_board(board)
+ end
